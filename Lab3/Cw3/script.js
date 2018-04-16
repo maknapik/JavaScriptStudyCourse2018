416c3037ddce85aa1a1b77c643a2cc7ab92d18de
MateuszKnapik <m.knapik6@gmail.com>
2018-04-16 14:00:48 +0200

    Cw3 add new version

--- Lab3/Cw3/script.js
+++ Lab3/Cw3/script.js
@@ -1,4 +1,4 @@
-var variable = 5
+let variable = 5
 
 function update()
 {
@@ -7,10 +7,19 @@ function update()
 /****************************************/
 function action()
 {
-    var el = document.getElementsByTagName("span")
-    for(i = 0 ; i < el.length ; i++)
+    let div1 = document.getElementById("d1")
+    let div2 = document.getElementById("d2")
+    let el1 = div1.getElementsByTagName("span")
+    let el2 = div2.getElementsByTagName("span")
+    let v = variable
+    for(let i = 0 ; i < el1.length ; i++)
     {
-        el[i].textContent = variable
+        el1[i].textContent = variable
+    }
+    for(let i = el2.length - 1 ; i >= 0 & v > 0 ; i--)
+    {
+        el2[i].textContent = parseInt(v % 10)
+        v /= 10;
     }
     if(variable == 0)
     {
